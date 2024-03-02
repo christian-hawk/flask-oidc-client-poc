457a8a3ed1e22be7882aed6a096a1a0275a13090
Christian Eland <eland.christian@gmail.com>
2024-03-02 01:09:54 +0000

    feat(logging): add logging to session handler

--- oidc_client/helpers/session_handler.py
+++ oidc_client/helpers/session_handler.py
@@ -1,16 +1,24 @@
 from flask.sessions import SessionMixin
+import logging
+
+logger = logging.getLogger(__name__)
 
 def is_authenticated_session(session: SessionMixin) -> bool:
+  logger.info('is_authenticated_session called')
   if 'id_token' in session.keys():
+    logger.debug('session is authenticated')
     return True
   else:
+    logger.debug('session is not authenticated')
     return False
   
 def clear_auth_session(session: SessionMixin) -> SessionMixin:
+  logger.info('clear_auth_session called')
   session.pop('user')
   session.pop('id_token')
   return session
   
 def get_token_hint(session: SessionMixin) -> str:
+  logger.info('get_token_hint called')
   token_hint = session.get('id_token')
   return token_hint
