b395faebdd75232fd68b86eea6c26d568460bc20
Christian Eland <eland.christian@gmail.com>
2024-03-11 15:22:37 +0000

    refactor(logging): add static defaults

--- oidc_client/helpers/setup_logging.py
+++ oidc_client/helpers/setup_logging.py
@@ -1,15 +1,22 @@
-
 import logging
 import sys
 
+DEFAULT_DATEFMT: str = '[%Y-%m-%d %H:%M:%S %z]'
+DEFAULT_FORMAT: str = '%(asctime)s [%(process)s] [%(levelname)s] %(filename)s:%(funcName)s %(message)s'
+DEFAULT_LEVEL: int = logging.INFO
+DEFAULT_HANDLERS: list = [ logging.StreamHandler(sys.stderr) ]
+
+
 def setup_logging(
-    format: str = '[%(asctime)s] %(levelname)s in %(module)s: %(message)s', 
-    datefmt: str = '%m/%d/%Y %I:%M:%S %p',
-    level: int = logging.DEBUG,
-    handlers: list = [ logging.StreamHandler(sys.stdout) ]) -> None:
+    format: str = DEFAULT_FORMAT, 
+    datefmt: str = DEFAULT_DATEFMT,
+    level: int = DEFAULT_LEVEL,
+    handlers: list = DEFAULT_HANDLERS
+    ) -> None:
   
     logging.basicConfig(
         format=format, 
         datefmt=datefmt, 
         level=level,
-        handlers=handlers)
+        handlers=handlers
+        )
--- tests/logging_test.py
+++ tests/logging_test.py
@@ -1,5 +1,6 @@
 import unittest.mock as mock
 from oidc_client.helpers.setup_logging import setup_logging
+from oidc_client.helpers import setup_logging as setup_logging_file
 import logging
 import sys
 
@@ -18,22 +19,20 @@ def test_setup_logging_called_with_params(mock_basicConfig):
                                             level=logging.INFO)
   
 
-
 @mock.patch('logging.basicConfig')
 def test_setup_logging_defaults_no_args(mock_basicConfig):
   # TODO: Test handler mocking Streamhandler
-  
-  default_format : str =  '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
-  default_datefmt : str = '%m/%d/%Y %I:%M:%S %p'
-  default_level :  int = logging.DEBUG
 
+  default_format = setup_logging_file.DEFAULT_FORMAT
+  default_datefmt = setup_logging_file.DEFAULT_DATEFMT
+  default_level = setup_logging_file.DEFAULT_LEVEL
+  default_handlers = setup_logging_file.DEFAULT_HANDLERS
   
-  # Call function with no arguments
-  setup_logging()
+  setup_logging_file.setup_logging()
 
   mock_basicConfig.assert_called_once_with(format=default_format,
-                                           datefmt=default_datefmt,
-                                           level=default_level,
-                                           handlers = mock.ANY
-                                           )
+                                          datefmt=default_datefmt,
+                                          level=default_level,
+                                          handlers = default_handlers
+                                          )
 
